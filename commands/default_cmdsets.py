b9d6ac6519f0e98398bf49aa904f7ccdc727d41c
Devil <69057727+DasBluEyedDevil@users.noreply.github.com>
2023-10-15 15:31:57 -0400

    Update default_cmdsets.py

--- commands/default_cmdsets.py
+++ commands/default_cmdsets.py
@@ -96,13 +96,13 @@ class SessionCmdSet(default_cmds.SessionCmdSet):
         # any commands you add below will overload the default ones.
         #
 
-class CommandSet(default_cmds.MuxCommandSet):
+class CommandSet(default_cmds.MuxCommand):
     def at_cmdset_creation(self):
         super().at_cmdset_creation()
         self.add(CmdGrantStatus())
         self.add(CmdRevokeStatus())
 
-class CustomCommandSet(default_cmds.MuxCommandSet):
+class CustomCommandSet(default_cmds.MuxCommand):
     def at_cmdset_creation(self):
         super().at_cmdset_creation()
         self.add(CmdSetStatus())
